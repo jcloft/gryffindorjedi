7a3888ed56f5182bc2235079f72e37757e09f135
calebamueller <calebamueller@gmail.com>
2015-09-24 15:30:54 -0500

    Update slate_connection.php

--- slate_connection.php
+++ slate_connection.php
@@ -1,22 +1,14 @@
 <?php
 // DB connection info
-$host = "tcp:slateproject.database.windows.net";
-$user = "jedi";
-$pwd = "4Gryffindor!";
-$db = "slate";
-try{
-    $conn = new PDO( "mysql:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
-}
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
+$servername = "tcp:slateproject.database.windows.net";
+$username = "jedi";
+$password = "4Gryffindor!";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
