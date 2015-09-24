a4851d34bb2379c3d26993c8aa82755540310f0a
calebamueller <calebamueller@gmail.com>
2015-09-24 15:29:10 -0500

    Update slate_connection.php

--- slate_connection.php
+++ slate_connection.php
@@ -1,3 +1,22 @@
 <?php
-echo test;
+// DB connection info
+$host = "tcp:slateproject.database.windows.net";
+$user = "jedi";
+$pwd = "4Gryffindor!";
+$db = "slate";
+try{
+    $conn = new PDO( "mysql:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
 ?>
