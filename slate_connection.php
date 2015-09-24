c1ad72c546aaf54f5fc41854bc4844e406c8b25b
calebamueller <calebamueller@gmail.com>
2015-09-24 15:46:12 -0500

    Update slate_connection.php

--- slate_connection.php
+++ slate_connection.php
@@ -1,10 +1,9 @@
   $serverName = "tcp:slateproject.database.windows.net,1433";
-   $userName = 'jedi';
+   $userName = 'jedi@slateproject.database.windows.net';
    $userPassword = '4Gryffindor!';
    $dbName = "slate";
-   $table = "slate";
 
-   $connectionInfo = array(UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
+   $connectionInfo = array("Database"=>$dbName, UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
 
    sqlsrv_configure('WarningsReturnAsErrors', 0);
    $conn = sqlsrv_connect( $serverName, $connectionInfo);
