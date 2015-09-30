e6142b833661d83d3c92392fc687ef0eba43155f
calebamueller <calebamueller@gmail.com>
2015-09-30 16:51:42 -0500

    Create slate_connection.php

---
+++ slate_connection.php
@@ -0,0 +1,13 @@
+<?php
+$serverName = "tcp:slateproject.database.windows.net, 1433";
+$connectionOptions = array("Database" => "slate", 
+                           "UID" => "jedi@slateproject",
+                           "PWD" => "4Gryffindor!");
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+ 
+if($conn){
+  echo "connected";
+  sqlsrv_query($conn,"INSERT INTO users (username, password_hash, pwsalt, salt) VALUES ('caleb', 'testhash', 'testpwsalt', 100)");
+  echo "inserted";
+}
+?>
