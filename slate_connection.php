e36bd593983f3d4b5dfae195e251f391a1a3095f
calebamueller <calebamueller@gmail.com>
2015-09-30 13:43:39 -0500

    Update slate_connection.php

--- slate_connection.php
+++ slate_connection.php
@@ -7,7 +7,7 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
  
 if($conn){
   echo "connected";
-  sqlsrv_query($conn,"INSERT INTO slate (username, password_hash, pwsalt, salt) VALUES ('caleb', 'testhash', 'testpwsalt', 100)")) !== false);
+  sqlsrv_query($conn,"INSERT INTO slate (username, password_hash, pwsalt, salt) VALUES ('caleb', 'testhash', 'testpwsalt', 100)");
   echo "inserted";
 }
 ?>
