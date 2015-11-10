2918cb414cea459530628b7ee10a639c30b1a7df
Ubuntu <jedi@slateproject.slateproject.g3.internal.cloudapp.net>
2015-11-10 22:50:17 +0000

    added jakes new files

--- application/views/index.php
+++ application/views/index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="webApp">
     <head>
+        <title>Home</title>
+        
+        <link rel=stylesheet href="/assets/css/homeStyle.css">
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-                <!-- Latest compiled and minified CSS -->
+
+        <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
         <!-- Optional theme -->
@@ -10,52 +15,25 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-        <link rel="stylesheet" href="http://slateproject.me/gryffindorjedi/assets/css/style.css">
+        
+        <link rel="stylesheet" href="assets/css/style.css">
         <link href='https://fonts.googleapis.com/css?family=Exo+2:400,200' rel='stylesheet' type='text/css'>
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-route.min.js"></script>
-
-
+        
+        <script src="/assets/js/angular.js"></script>
+        
     </head>
-
+    
     <body>
         <div class="container-fluid">
-            <div class="wrapper">
-                <div class="col-xs-12 col-sm-8" id="bgleft"></div>
-                <div class="col-xs-12 col-sm-4 col-sm-offset-8" id="bgright"></div>
-                    <div class="col-xs-12 col-sm-8" id="left">
-                        <p class="row" id="title">SLATE</p>
-                        <div class="row" id="caption">
-                            <div class="col-xs-12 col-sm-6">
-                                <h2><a href="#">For Teachers</a></h2>
-                            </div>
-                            <div class="col-xs-12 col-sm-6">
-                                <h2><a href="#">For Students</a></h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-4" id="right">
-                        <form action="/home" class="row col-xs-12 col-sm-10 col-sm-offset-1" id="signin">
-                        <h1>Sign-In</h1>
-                          <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                          </div>
-                          <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                          </div>
-                          <button type="submit" class="btn btn-default">Sign-in</button>
-                        </form>
-                        <div class="row col-xs-12" id="signup">
-                            <h3>Need an account?<br><a href="#">Sign Up</a></h3>
-                        </div>
-
-                    </div>
+            <div class="wrapper col-sm-12">
+                    <div ng-view></div>
             </div>
         </div>
-    </body>
+        
 
+    </body>
 </html>
