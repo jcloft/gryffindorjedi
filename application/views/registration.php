c436afec34a21c7eedc40e551139d91f0a52e4ce
B Esmond <bsey7c@mail.missouri.edu>
2015-11-11 16:16:19 -0600

    Updates
    -Qui-Gon Jinn

--- application/views/registration.php
+++ application/views/registration.php
@@ -3,20 +3,20 @@
 	$form_attributes = array('class' => 'register', 'id' => 'register');
 	echo form_open('logincontrol/register', $form_attributes);
 	$user_data = array('name' => 'username',
-						'id' => 'username',
-						'placeholder' => 'Desired Username'
-						);
+						         'id' => 'username',
+						         'placeholder' => 'Desired Username'
+						  );
 	$password_data = array('name' => 'password',
-							'id' => 'password',
-							'placeholder' => 'Password'
+							           'id' => 'password',
+							           'placeholder' => 'Password'
 							);
 	$confirm_pass = array ('name' => 'confirm_pass',
-							'id' => 'confirm_pass',
-							'placeholder' => 'Confirm Password'
+							           'id' => 'confirm_pass',
+							           'placeholder' => 'Confirm Password'
 							);
 	$submit_data = array('name' => 'submit',
-						'id' => 'submit',
-						'value' => 'Register'
+						           'id' => 'submit',
+						           'value' => 'Register'
 						);
 	echo form_input($user_data);
 	echo form_password($password_data);
