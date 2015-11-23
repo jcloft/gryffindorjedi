964b1aa9f0a59b37f6d943fbb5c9d428f1e5b567
Ubuntu <jedi@slateproject.slateproject.g3.internal.cloudapp.net>
2015-11-23 00:28:15 +0000

    test

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<!html>
     <head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
                 <!-- Latest compiled and minified CSS -->
